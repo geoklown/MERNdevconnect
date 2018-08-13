aa59a3d6a719a1a85c7ff485e41c91f57db18349
John Junkins <jj@j6srvs.com>
2018-08-13 11:28:43 -0500

    Mongo DB connected

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 app.get('/', (req, res) => res.send('hi!'));
+// DB Config
+const db = require('./config/keys').mongoURI;
+// Connect to MongoDB
+mongoose.connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 const port = process.env.Port || 5000;
 app.listen(port, () => console.log(`server running on port ${port}`));
