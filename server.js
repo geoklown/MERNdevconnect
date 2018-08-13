90fbfed34da34f29d7f50426299327a218622a3b
John Junkins <jj@j6srvs.com>
2018-08-13 11:09:28 -0500

    typo fix app.listen....console.log

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('hello'));
 const port = process.env.Port || 5000;
-app.listen(port, () => consolelog(`server running on port ${port}`));
+app.listen(port, () => console.log(`server running on port ${port}`));
