c3d951c5817f2ffc21dc913e8d2ebca5198aa8f2
John Junkins <jj@j6srvs.com>
2018-08-13 17:17:22 -0500

    user registration req.body

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
+//Load user medel
+const User = require('../../modules/User');
 // @route Get Request api/users/tes
 // @desc Test users route
 // @access Public route
 router.get('/test', (req, res) => res.json({
   msg: "User Works"
 }));
+// @route Get Request api/users/register
+// @desc Register users
+// @access Public route
+router.post('/register', (req, res) => {
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    if (user) {
+      return res.status(400).json({
+        email: 'Email Already Exists '
+      });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar: avatar,
+        password: req.body.password
+      });
+    }
+  })
+});
 module.exports = router;
