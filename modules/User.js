a1360024fb3efc17e18c1021cb72582122c040f0
John Junkins <jj@j6srvs.com>
2018-08-13 17:28:46 -0500

    Gravatar requirments

--- modules/User.js
+++ modules/User.js
@@ -16,7 +16,7 @@ const UserSchema = new Schema({
   },
   avatar: {
     type: String,
-    required: true
+
   },
   date: {
     type: Date,
--- routes/api/users.js
+++ routes/api/users.js
@@ -29,7 +29,7 @@ router.post('/register', (req, res) => {
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
-        avatar: avatar,
+        avatar,
         password: req.body.password
       });
     }
