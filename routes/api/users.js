fc200195415130ff35f31b909d414198ee9746d8
John Junkins <jj@j6srvs.com>
2018-08-16 11:52:46 -0500

    Login validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,7 @@ const keys = require('../../config/keys');
 const passport = require('passport');
 // Load input Validation
 const validateResgisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 //Load user model
 const User = require('../../modules/User');
 // @route Get Request api/users/tes
@@ -62,6 +63,14 @@ router.post('/register', (req, res) => {
 // @desc Login User/ Return JWT Token
 // @access Public route
 router.post('/login', (req, res) => {
+  const {
+    errors,
+    isValid
+  } = validateLoginInput(req.body);
+  // Check if valid
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   const email = req.body.email;
   const password = req.body.password;
   // Find user by email
@@ -71,9 +80,10 @@ router.post('/login', (req, res) => {
     .then(user => {
       // Check for user
       if (!user) {
-        return res.status(404).json({
-          email: 'User not found'
-        });
+        errors.email = 'User not found';
+        return res.status(404).json(
+          errors
+        );
       }
       // Check Password
       bcrypt.compare(password, user.password).then(isMatch => {
@@ -95,9 +105,10 @@ router.post('/login', (req, res) => {
 
           });
         } else {
-          return res.status(400).json({
-            password: 'Password Incorrect'
-          });
+          errors.password = 'Password Incorrect';
+          return res.status(400).json(
+            errors
+          );
         }
       })
     });
