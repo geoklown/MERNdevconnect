2fb8a0ae5cb7454b7eaf6bc7eb2747557fb3ebe8
John Junkins <jj@j6srvs.com>
2018-08-20 11:17:25 -0500

    experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -175,4 +175,31 @@ router.post(
   }
 );
 
+// @route POST Request api/profile/experience
+// @desc ADD experience profile
+// @access Private
+router.post('/experience', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  Profile.findOne({
+    user: req.user.id
+  }).then(profile => {
+    const newExp = {
+      title: req.body.title,
+      company: req.body.company,
+      location: req.body.location,
+      from: req.body.from,
+      to: req.body.to,
+      current: req.body.current,
+      description: req.body.description
+    }
+    // Add to Experience Array
+    profile.experience.unshift(newExp);
+    profile.save().then(profile => res.json(profile));
+  })
+});
+
+
+
+
 module.exports = router;
