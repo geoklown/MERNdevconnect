05e72e52b10b82f323fa07d9689a92e308b44a3b
John Junkins <jj@j6srvs.com>
2018-08-20 16:43:57 -0500

    delete education and experience routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -243,5 +243,49 @@ router.post('/education', passport.authenticate('jwt', {
   })
 });
 
+// @route POST Request api/profile/experience/:exp_id
+// @desc Delete Experience from Profile
+// @access Private
+router.delete('/experience/:exp_id', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+
+  Profile.findOne({
+      user: req.user.id
+    }).then(profile => {
+      // Get remove Index
+      const removeIndex = profile.experience
+        .map(item => item.id)
+        .indexOf(req.params.exp_id);
+      // Splice out of array
+      profile.experience.splice(removeIndex, 1);
+      // save
+      profile.save().then(profile => res.json(profile));
+    })
+    .catch(err => res.status.json(err));
+});
+
+// @route POST Request api/profile/education/:edu_id
+// @desc Delete Education from Profile
+// @access Private
+router.delete('/education/:edu_id', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+
+  Profile.findOne({
+      user: req.user.id
+    }).then(profile => {
+      // Get remove Index
+      const removeIndex = profile.education
+        .map(item => item.id)
+        .indexOf(req.params.edu_id);
+      // Splice out of array
+      profile.education.splice(removeIndex, 1);
+      // save
+      profile.save().then(profile => res.json(profile));
+    })
+    .catch(err => res.status.json(err));
+});
+
 
 module.exports = router;
