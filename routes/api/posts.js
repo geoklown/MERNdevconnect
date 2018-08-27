9ba6b4f14d0a3ce46a2fa5f4e8024dd28fd9d604
John Junkins <jj@j6srvs.com>
2018-08-27 11:41:11 -0500

    posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,10 +2,27 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
+const Post = require('../../models/Post')
 // @route Get Request api/post/tes
 // @desc Test Post route
 // @access Public route
 router.get('/test', (req, res) => res.json({
   msg: "Posts Works"
 }));
+// @route Post Request api/posts
+// @desc Create Post
+// @access Private route
+
+router.post('/', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  const newPost = new Post({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.name,
+    user: req.user.id
+  });
+  newPost.save().then(post => res.json(post));
+});
+
 module.exports = router;
