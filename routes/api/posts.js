f7f54cbb77062cb0cd4e3efa1f931d42f8a854c1
John Junkins <jj@j6srvs.com>
2018-08-27 11:34:18 -0500

    Post.js filename change

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 // @route Get Request api/post/tes
 // @desc Test Post route
 // @access Public route
