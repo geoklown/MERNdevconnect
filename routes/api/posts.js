60c21a62a4ae2a961b8eb72617eb82a062548c52
John Junkins <jj@j6srvs.com>
2018-08-27 12:23:40 -0500

    Postman validate

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
+// Post Model
 const Post = require('../../models/Post')
+// Post Validation
+const validatePostInput = require('../../validation/post');
 // @route Get Request api/post/tes
 // @desc Test Post route
 // @access Public route
@@ -16,10 +19,19 @@ router.get('/test', (req, res) => res.json({
 router.post('/', passport.authenticate('jwt', {
   session: false
 }), (req, res) => {
+  const {
+    errors,
+    isValid
+  } = validatePostInput(req.body);
+  // Check Validation
+  if (!isValid) {
+    // If any errors send 400 with errors object
+    return res.status(400).json(errors);
+  }
   const newPost = new Post({
     text: req.body.text,
     name: req.body.name,
-    avatar: req.body.name,
+    avatar: req.body.avatar,
     user: req.user.id
   });
   newPost.save().then(post => res.json(post));
