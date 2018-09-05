020e6d45c784fc4389f060d564a632cb17008702
John Junkins <jj@j6srvs.com>
2018-09-05 12:18:42 -0500

    Add comment on post ref #13

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -11,9 +11,11 @@ const validatePostInput = require('../../validation/post');
 // @route Get Request api/post/tes
 // @desc Test Post route
 // @access Public route
-router.get('/test', (req, res) => res.json({
-  msg: "Posts Works"
-}));
+router.get('/test', (req, res) =>
+  res.json({
+    msg: 'Posts Works'
+  })
+);
 // @route Get Request api/posts
 // @desc Get Posts
 // @access Public
@@ -21,124 +23,184 @@ router.get('/', (req, res) => {
   Post.find()
     .sort({
       date: -1
-    }).then(posts => res.json(posts)).catch(err => res.status(404).json({
-      nopostfound: 'I don\'t have that'
-    }));
+    })
+    .then(posts => res.json(posts))
+    .catch(err =>
+      res.status(404).json({
+        nopostfound: "I don't have that"
+      })
+    );
 });
 // @route Get single Request api/posts/:id
 // @desc Get Posts by Id
 // @access Public
 router.get('/:id', (req, res) => {
   Post.findById(req.params.id)
-    .then(post => res.json(post)).catch(err => res.status(404).json({
-      nopostfound: 'I don\'t have that'
-    }));
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({
+        nopostfound: "I don't have that"
+      })
+    );
 });
 // @route DELETE api/posts/:id
 // @desc delete Posts by Id
 // @access Private
-router.delete('/:id', passport.authenticate('jwt', {
-  session: false
-}), (req, res) => {
-  Profile.findOne({
-    user: req.user.id
-  }).then(profile => {
-    Post.findById(req.params.id)
-      .then(post => {
-        // check for post owner
-        if (post.user.toString() !== req.user.id) {
-          return res.status(401).json({
-            notauthorized: 'User not authorized'
-          });
-        }
-        // Delete Post
-        post.remove().then(() => res.json({
-          success: true
-        }));
-      })
-      .catch(err => res.status(404).json({
-        nopostfound: 'I don\t have that'
-      }));
-  })
-});
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', {
+    session: false
+  }),
+  (req, res) => {
+    Profile.findOne({
+      user: req.user.id
+    }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({
+              notauthorized: 'User not authorized'
+            });
+          }
+          // Delete Post
+          post.remove().then(() =>
+            res.json({
+              success: true
+            })
+          );
+        })
+        .catch(err =>
+          res.status(404).json({
+            nopostfound: 'I don\t have that'
+          })
+        );
+    });
+  }
+);
 // @route Post Request api/posts/like/:id
 // @desc Like Post
 // @access Private
-router.post('/like/:id', passport.authenticate('jwt', {
-  session: false
-}), (req, res) => {
-  Profile.findOne({
-    user: req.user.id
-  }).then(profile => {
-    Post.findById(req.params.id)
-      .then(post => {
-        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
-          return res.status(400).json({
-            alreadyliked: 'User already liked this post'
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', {
+    session: false
+  }),
+  (req, res) => {
+    Profile.findOne({
+      user: req.user.id
+    }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res.status(400).json({
+              alreadyliked: 'User already liked this post'
+            });
+          }
+          // Add user id to likes array
+          post.likes.unshift({
+            user: req.user.id
           });
-        }
-        // Add user id to likes array
-        post.likes.unshift({
-          user: req.user.id
-        });
-        post.save().then(post => res.json(post));
-      })
-      .catch(err => res.status(404).json({
-        nopostfound: 'I don\t have that'
-      }));
-  })
-});
+          post.save().then(post => res.json(post));
+        })
+        .catch(err =>
+          res.status(404).json({
+            nopostfound: 'I don\t have that'
+          })
+        );
+    });
+  }
+);
 // @route Post Request api/posts/unlike/:id
 // @desc UnLike Post
 // @access Private
-router.post('/unlike/:id', passport.authenticate('jwt', {
-  session: false
-}), (req, res) => {
-  Profile.findOne({
-    user: req.user.id
-  }).then(profile => {
-    Post.findById(req.params.id)
-      .then(post => {
-        if (post.likes.filter(like => like.user.toString() === req.user.id).length == 0) {
-          return res.status(400).json({
-            notliked: 'You have not liked this post yet'
-          });
-        }
-        // Get remove index
-        const removeIndex = post.likes.map(item => item.user.toString()).indexOf(req.user.id);
-        // Splice out of array
-        post.likes.splice(removeIndex, 1);
-        // Save
-        post.save().then(post => res.json(post));
-      })
-      .catch(err => res.status(404).json({
-        nopostfound: 'I don\t have that'
-      }));
-  })
-});
+router.post(
+  '/unlike/:id',
+  passport.authenticate('jwt', {
+    session: false
+  }),
+  (req, res) => {
+    Profile.findOne({
+      user: req.user.id
+    }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length == 0
+          ) {
+            return res.status(400).json({
+              notliked: 'You have not liked this post yet'
+            });
+          }
+          // Get remove index
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+          // Splice out of array
+          post.likes.splice(removeIndex, 1);
+          // Save
+          post.save().then(post => res.json(post));
+        })
+        .catch(err =>
+          res.status(404).json({
+            nopostfound: 'I don\t have that'
+          })
+        );
+    });
+  }
+);
 // @route Post Request api/posts
 // @desc Create Post
 // @access Private route
 
-router.post('/', passport.authenticate('jwt', {
-  session: false
-}), (req, res) => {
-  const {
-    errors,
-    isValid
-  } = validatePostInput(req.body);
-  // Check Validation
-  if (!isValid) {
-    // If any errors send 400 with errors object
-    return res.status(400).json(errors);
+router.post(
+  '/',
+  passport.authenticate('jwt', {
+    session: false
+  }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    // Check Validation
+    if (!isValid) {
+      // If any errors send 400 with errors object
+      return res.status(400).json(errors);
+    }
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+    newPost.save().then(post => res.json(post));
   }
-  const newPost = new Post({
-    text: req.body.text,
-    name: req.body.name,
-    avatar: req.body.avatar,
-    user: req.user.id
-  });
-  newPost.save().then(post => res.json(post));
-});
+);
+// @route Post api/posts/comment/:id
+// @desc Add Comment to post
+// @access private
+
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+        // add to comments array
+        post.comments.unshift(newComment);
+        // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'No Post Found' }));
+  }
+);
 
 module.exports = router;
