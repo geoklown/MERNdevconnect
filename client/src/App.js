aaf849e140cc1da46458cdaaf0943a0b6f69f07d
John Junkins <jj@j6srvs.com>
2018-09-07 12:27:18 -0500

    missing merge

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My react App v2</h1>
+        <h1>My react App v2 This is where all the cool stuff is going to go</h1>
+        <h2>Once i figure it all out</h2>
       </div>
     );
   }
