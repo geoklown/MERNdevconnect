3a44fbf3c364c3629c8d3a0296586eced3befc2e
John Junkins <jj@j6srvs.com>
2018-09-10 10:34:45 -0500

    Bringing in react-router-dom

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -9,11 +9,11 @@ class Landing extends Component {
             <div className="container">
               <div className="row">
                 <div className="col-md-12 text-center">
-                  <h1 className="display-3 mb-4">Developer Connector</h1>
+                  <h1 className="display-3 mb-4">Junkins Connector</h1>
                   <p className="lead">
                     {' '}
-                    Create a developer profile/portfolio, share posts and get
-                    help from other developers
+                    Create a Junkins profile/portfolio, share posts and get help
+                    from other Junkins
                   </p>
                   <hr />
                   <a href="register.html" className="btn btn-lg btn-info mr-2">
