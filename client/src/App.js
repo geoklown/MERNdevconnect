b6405bf501ec1c753f20c1b1ac50b31bf2f2c717
John Junkins <jj@j6srvs.com>
2018-09-10 10:58:28 -0500

    Add Routes to layout components navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/register';
+import Login from './components/auth/login';
 import './App.css';
 
 class App extends Component {
@@ -11,7 +13,11 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
-          <Landing />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/Register" component={Register} />
+            <Route exact path="/Login" component={Login} />
+          </div>
           <Footer />
         </div>
       </Router>
