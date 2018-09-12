143eef018f9147e6685051045497de61753647ac
John Junkins <jj@j6srvs.com>
2018-09-12 12:58:28 -0500

    Move store from app.js to store.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import store from './store';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -9,7 +9,6 @@ import Register from './components/auth/register';
 import Login from './components/auth/login';
 import './App.css';
 
-const store = createStore(() => [], applyMiddleware());
 class App extends Component {
   render() {
     return (
